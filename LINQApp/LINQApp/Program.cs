a09bcac253a06ef6e8fb54382d9d88b6d5c3fd82
Ha-Mim <hamim.hyder@gmail.com>
2015-02-14 22:24:11 +0600

    Added

---
+++ LINQApp/LINQApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, -2, 3, 0, -4, 5 };
+            // Create a query that obtains only positive numbers.
+            var posNums = from n in nums
+                          where n > 0
+                          select n;
+            Console.Write("The positive values in nums: ");
+            // Execute the query and display the results.
+            foreach (int i in posNums) Console.Write(i + " ");
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
